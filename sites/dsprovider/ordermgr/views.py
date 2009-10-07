2a57312ff6f4f06a5c7e4f6f2bd17d3918435bf4
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-07 16:11:09 +0300

    made use of login_required so that users could login to application some small views code cleanup

--- sites/dsprovider/ordermgr/views.py
+++ sites/dsprovider/ordermgr/views.py
@@ -1,19 +1,7 @@
-# Create your views here.
-from datetime import datetime 
-
-from django.db.models import Q
-from django.http import HttpResponseRedirect, HttpResponse
 from django.core.exceptions import PermissionDenied, ObjectDoesNotExist, ImproperlyConfigured
-from django.core.urlresolvers import reverse
-from django.forms.models import modelformset_factory
 from django.template import RequestContext
-from django.views.generic.simple import direct_to_template
-
-
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth.decorators import login_required
 
 from models import UserProfile
 from models import KitchenDesignRequest as DesignOrder
@@ -38,71 +26,79 @@ def get_context(request, orderid=None):
         if orderid:
             try:
                 order = DesignOrder.objects.get(pk=orderid)
-            except ObjectDoesNotExist as ex: 
+            except ObjectDoesNotExist as ex:
                 log.error( "failed to find the orderid %d" % orderid )
                 raise ImproperlyConfigured("Access to order %s denied to user %s" % (orderid, user))
     else:
         log.error( "unathenticated user in get_context" )
         raise PermissionDenied()
-    
+
     return (user, account, profile, order)
 
 
+@login_required
 def dashboard(request):
     """
     Dashboard page for designers
-    
+
     Display a list of orders related to this designer, combined with a list of unclaimed orders.
     """
-    user,account,profile,product = get_context(request)
-    
-    pending = DesignOrder.objects.filter( status=STATUS_NEW )
-        
-    working = DesignOrder.objects.filter( status=STATUS_ASSIGNED )
-    
-    completed = DesignOrder.objects.filter( status=STATUS_COMPLETED )
+    user, account, profile, product = get_context(request)
+
+    pending = DesignOrder.objects.filter(status=STATUS_NEW)
+
+    working = DesignOrder.objects.filter(status=STATUS_ASSIGNED)
+
+    completed = DesignOrder.objects.filter(status=STATUS_COMPLETED)
     # except:
     #     orders = []
-        
-    stats = { 'headers': ['Today', 'This Week', 'This Month'], 
+
+    stats = { 'headers': ['Today', 'This Week', 'This Month'],
               'arrived': [3, 25, 84],
               'completed': [4, 22, 80],
             }
-            
-    return render_to_response( 'designer/dashboard.html', 
-                            { 'account':account, 'pending':pending, 'working': working, 
-                              'completed': completed, 'stats':stats },
-                            context_instance=RequestContext(request) ) 
+
+    return render_to_response( 'designer/dashboard.html', {
+        'account': account,
+        'pending': pending,
+        'working': working,
+        'completed': completed,
+        'stats': stats,
+    }, context_instance=RequestContext(request) )
 
 
+@login_required
 def manage_orders(request):
     pass
-    
+
+@login_required
 def manage_designers(request):
     pass
-    
+
+@login_required
 def manage_designer(request):
     pass
-        
-            
+
+
+@login_required
 def display_order(request, orderid):
     """
     Display a read-only view of the design request order, intended for designers.
-    
-    TODO: Printer CSS    
-    """    
+
+    TODO: Printer CSS
+    """
     # validate access
-    user, account, profile, order = get_context(request,orderid)
+    user, account, profile, order = get_context(request, orderid)
     # disable control if not yet assigned
     if not order.status == STATUS_ASSIGNED:
         disabled='disabled=disabled'
     else:
         disabled=''
-    
+
     errors = None
     # setup forms
     if request.method== 'GET':
-        package_form = PackageForm(instance=order) 
+        package_form = PackageForm(instance=order)
     elif request.method == 'POST':
         # use presence of FILES to distinguish between design update and 'complete' actions
         if request.FILES:
@@ -114,7 +110,7 @@ def display_order(request, orderid):
             if 'complete-order-action' in request.POST:
                 if order.package:
                     order.complete(user)
-                    return HttpResponseRedirect(reverse('ordermgr.views.dashboard'))
+                    return redirect('ordermgr.views.dashboard')
                 else:
                     log.warning( "Attempt to complete order without attached design(s)" )
                     errors = "Cannot complete design order without attached design package"
@@ -130,16 +126,18 @@ def display_order(request, orderid):
     else:
         log.error( "Illegal HTTP Operation %s" % request.method )
         raise Exception, "Illegal HTTP Operation %s" % request.method
-            
+
     # render template
-    return render_to_response( 
-                'designer/display_order.html', { 
-                    'order':order, 
+    return render_to_response(
+                'designer/display_order.html', {
+                    'order':order,
                     'options':order.display_as_optional,
                     'disabled':disabled,
-                }, 
+                },
                 context_instance=RequestContext(request) )
-    
+
+
+@login_required
 def claim_order(request, orderid):
     """
     Claim an order for the current (logged in) designer.
@@ -153,20 +151,22 @@ def claim_order(request, orderid):
     else:
         log.error( 'Order %s is already assigned to %s - cannot reassign' % (orderid, user) )
         raise Exception, 'Order %s is already assigned to %s - cannot reassign' % (orderid, user)
-            
-    return HttpResponseRedirect(reverse('ordermgr.views.dashboard'))
 
-def assign_order(request, orderid ):
+    return redirect('ordermgr.views.dashboard')
+
+
+@login_required
+def assign_order(request, orderid):
     """
     Create an order assignment by presenting a list of available (only?) designers for selection
-    """    
+    """
     # get/validate selected order is unassinged (new)
     user, account, profile, order = get_context(request,orderid)
     # make sure user has right to assign orders
     if not user.is_staff:
         log.info('assign_order: assign order permission denied for user %s' % user )
         raise PermissionDenied
-    # make sure order is assignable 
+    # make sure order is assignable
     if order.is_assigned():
         log.error( 'Order %s is already assigned to %s - cannot reassign' % (orderid, designer) )
         raise PermissionDenied
@@ -179,28 +179,30 @@ def assign_order(request, orderid ):
         form = AssignDesignerForm(request.POST, instance=order)
         if form.is_valid():
             instance = form.save(commit=False)
-            instance.assign_designer(instance.designer) # designer was assigned by form..            
-            return HttpResponseRedirect(reverse('ordermgr.views.dashboard'))
+            instance.assign_designer(instance.designer) # designer was assigned by form..
+            return redirect('ordermgr.views.dashboard')
         else:
             log.warning('assign_order: unexpected state - invalid form' )
     else:
         log.error('invalid HTTP method in assign_order: %s' % request.method )
         raise Exception, "Invalid request type %s" % request.method
 
-    return render_to_response('designer/assign_order.html', locals(),                
-                context_instance=RequestContext(request) )        
+    return render_to_response('designer/assign_order.html', locals(),
+                context_instance=RequestContext(request) )
 
+@login_required
 def clarify_order(request,orderid):
     pass
-    
+
+@login_required
 def attach_design_to_order(request,orderid):
-    
+
     # get/validate selected order is unassinged (new)
     user, account, profile, order = get_context(request,orderid)
-    # 
+    #
     attachments = order.orderattachment_set.filter(source__exact=1)
     if request.method == 'GET':
-        form = PackageForm() 
+        form = PackageForm()
     elif request.method == 'POST':
         form = PackageForm(request.POST,request.FILES)
         if form.is_valid():
@@ -210,24 +212,26 @@ def attach_design_to_order(request,orderid):
             doc.method = 0
             doc.user = user
             doc.org = account
-            doc.save()        
-            return HttpResponseRedirect(reverse(display_order, args=[orderid]))    
+            doc.save()
+            return redirect(display_order, args=[orderid])
     else:
         log.error('Invalid HTTP method in attach_design_to_order: %s' % request.method )
         raise Exception, "Invalid request type %s" % request.method
-    
+
     # render template
     return render_to_response('designer/attach_design.html', locals(),
                 context_instance=RequestContext(request) )
-            
+
+@login_required
 def submit_order(request, orderid):
     pass
 
+@login_required
 def complete_order(request, orderid):
     # get/validate selected order is unassinged (new)
     user, account, profile, order = get_context(request,orderid)
     try:
         order.complete(user)
-        return HttpResponseRedirect(reverse(dashboard))
+        return redirect(dashboard)
     except:
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        return redirect(request.META['HTTP_REFERER'])
