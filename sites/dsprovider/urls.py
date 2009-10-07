8b7da57a3193208152db5acdb787878255e5ce9e
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-07 16:09:03 +0300

    removed 'oneworld' prefix in urls

--- sites/dsprovider/urls.py
+++ sites/dsprovider/urls.py
@@ -1,22 +1,21 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
+from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^oneworld/', include("ordermgr.urls")),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
 
-from django.conf import settings
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
     )
+
+urlpatterns += patterns('',
+    (r'^', include("ordermgr.urls")),
+)
