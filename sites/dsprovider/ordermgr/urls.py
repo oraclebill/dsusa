70d99a743c12ab68c24978816decd8685dce0bcb
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-07 16:09:46 +0300

    reordered ordermgr urls, renamed logout url

--- sites/dsprovider/ordermgr/urls.py
+++ sites/dsprovider/ordermgr/urls.py
@@ -1,21 +1,18 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('ordermgr', 
-                        (r'^$', 'views.dashboard'),
-                        (r'^order/(\d+)/display$', 'views.display_order'),
-                        (r'^order/(\d+)/assign$', 'views.assign_order'),
-                        (r'^order/(\d+)/claim$', 'views.claim_order'),
-                        (r'^order/(\d+)/clarify$', 'views.clarify_order'),
-                        (r'^order/(\d+)/attach$', 'views.attach_design_to_order'),
-                        (r'^order/(\d+)/complete$', 'views.complete_order'),
-                        )
-                        
-urlpatterns += patterns('django.contrib.auth',                         
-                        url(r'^login/$', 
-                            'views.login', 
-                            {'template_name':'login.html'} ),
-                        url(r'^logout/$', 
-                            'views.logout', 
-                            name='do-logout'),
-                       )
+urlpatterns = patterns('ordermgr',
+        (r'^$', 'views.dashboard'),
+        (r'^order/(\d+)/display$', 'views.display_order'),
+        (r'^order/(\d+)/assign$', 'views.assign_order'),
+        (r'^order/(\d+)/claim$', 'views.claim_order'),
+        (r'^order/(\d+)/clarify$', 'views.clarify_order'),
+        (r'^order/(\d+)/attach$', 'views.attach_design_to_order'),
+        (r'^order/(\d+)/complete$', 'views.complete_order'),
+)
 
+urlpatterns += patterns('django.contrib.auth',
+        url(r'^accounts/login/$', 'views.login', {
+                'template_name': 'login.html',
+        }, name='auth_login'),
+        url(r'^accounts/logout/$', 'views.logout', name='auth_logout'),
+)
