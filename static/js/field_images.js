a733f6d0606c3a336d03fe9399051ae6ea789452
Sidorenko Vladimir <yoyavova@gmail.com>
2009-11-08 02:48:03 +0200

    wizard: change fieldset image on checkbox and radiobutton change in safari.

--- static/js/field_images.js
+++ static/js/field_images.js
@@ -1,17 +1,19 @@
 $(function(){
-	$('#wizard_content :input:visible').focus(function(){
+    function set_image(element) {
 		var img = MEDIA_URL + 'wizard/'+STEP+'/';
-		
-		img += this.name
-		
-		var tag_type = $(this).attr('type')
-		if (tag_type == 'radio') {
-			img += '_' + $(this).val()
-		}
-		img += '.png'
 
-		var fs_id = get_fieldset_id(this)
+		img += this.name;
+
+		var tag_type = $(this).attr('type');
+		if (tag_type == 'radio') {
+			img += '_' + $(this).val();
+		}
+		img += '.png';
+
+		var fs_id = get_fieldset_id(this);
 		//console.info(img)
-		$('img#fieldset_img_'+fs_id).attr('src', img)
-	})
-})
+		$('img#fieldset_img_'+fs_id).attr('src', img);
+    }
+	$('#wizard_content input:visible').focus(set_image);
+	$('#wizard_content input[type=radio]:visible, #wizard_content input[type=checkbox]:visible').change(set_image);
+});
