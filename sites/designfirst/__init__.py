3b529f6ae9c128cd93ce08ce04d2f12327f96300
beta web tier <oraclebill@gmail.com>
2009-11-18 18:33:01 -0500

    removed exceptions from image upload, some cosmetics

--- sites/designfirst/__init__.py
+++ sites/designfirst/__init__.py
@@ -1,4 +1,4 @@
 import logging
-logging.basicConfig(format='%(asctime)s [%(thread)d] %(levelname)s: %(name)s:%(funcName)s: %(message)s')      
+logging.basicConfig(format='%(asctime)s [%(thread)d] %(levelname)s: %(name)s:%(funcName)s: %(message)s', level=logging.DEBUG)
 
 import management
