b12c0ffa3cf61f3b9109baa102094bee6b5f7bc4
Bill Jones <bill@averline.com>
2010-01-07 22:59:53 -0500

    un-specified fields are now grouped into a default fieldset in admin.

--- sites/designfirst/orders/admin.py
+++ sites/designfirst/orders/admin.py
@@ -1,40 +1,67 @@
 from models import WorkingOrder, Attachment, AttachmentPage
 from django.contrib import admin
+from django.forms.models import fields_for_model
 
 class AttachmentInline(admin.TabularInline):
+    
     model = Attachment
+    
 
 class AttachmentPageInline(admin.TabularInline):
+    
     model = AttachmentPage
+    
+    
+class OrderNoteInline(admin.ModelAdmin):
+    
+    model = OrderNote
+
+    list_display = ['author', 'subject', 'date']
+    inlines = [
+        AttachmentPageInline, 
+#        AttachmentPageInline  # causes hangs..
+    ]
+    
 
 class WorkingOrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'account_code', 'project_name', 'status', 'created', 'owner', 'updated', 'submitted' ]
-    list_display_links = ['id', 'project_name']
-    list_filter = ['owner', 'status' ]
+    
+    list_display = ['id', 'project_name', 'account_code', 'status', 'owner', 'created', 'submitted', 'updated', ]
+    list_display_links = ['id', 'account_code', 'project_name']
+    list_filter = ['owner', 'account_code', 'status' ]
     ordering = ['id',]
     search_fields = ['project_name',]
     #radio_fields = {'status': admin.HORIZONTAL, 'project_type': admin.HORIZONTAL }
     save_as = True
     actions_on_top = True
     
-#    fieldsets = (
-#        ('Order Information', {
-#            'fields': ('account_code', 'tracking_code', 
-#                       'project_name', 'owner', 'status',   
-#                       'project_type', 
-#                       ('rush', 'color_views', 'elevations', 'quoted_cabinet_list'), 'cost', )
+    fieldsets = (
+        ('Order Information', {
+            'fields': ( 'status', 'account_code', 'tracking_code', 
+                       'project_name', 'owner', 'project_type', 
+                       ('rush', 'color_views', 'elevations', 'quoted_cabinet_list'), 'cost', )
+        }),
+#        ('Order Status', {
+#            'fields': ('status', 'submitted', 'updated') 
 #        }),
-#    )
+    )
+
     inlines = [
         AttachmentInline, 
 #        AttachmentPageInline  # causes hangs..
     ]
     
-    def complete_order(self):
-        print "Yay!"
-
+    def __init__(self, model, admin_site):
+        super(WorkingOrderAdmin, self).__init__(model, admin_site)
+        fields = fields_for_model(self.model)            
+        for fieldname in admin.util.flatten_fieldsets(self.fieldsets):
+            fields.pop(fieldname, None) 
+        undeclared_fields = (('Design Information', 
+                                {'fields': fields.keys() ,
+                                 'classes': ('collapse',) } ),)
+        self.fieldsets += undeclared_fields
 
 class AttachmentAdmin(admin.ModelAdmin):
+    
     list_display = ['order', 'type', 'file', 'source', 'timestamp']
     inlines = [
         AttachmentPageInline, 
@@ -42,6 +69,7 @@ class AttachmentAdmin(admin.ModelAdmin):
     ]
     
 class AttachmentPageAdmin(admin.ModelAdmin):
+    
     list_display = ['id', 'attachment', 'page', 'file']
     
 admin.site.register(WorkingOrder, WorkingOrderAdmin)
